6dc8772dab937c9df74fde7042e4d5538073a8e6
bbbpppolly <bernard@weeklyhow.com>
2021-10-06 01:56:25 +0200

    Lesson: [Product] - Image Carousels

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        '94': '22rem'
+      }
+    },
   },
   variants: {
     extend: {},
